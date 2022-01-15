7dbcf21679ae70fdba3ee539f97da73953dd97f0
MohammadHafez Yari <hafez.yari33@gmail.com>
2022-01-15 15:42:01 +0330

    Add RGB output and optimize code

--- main.cpp
+++ main.cpp
@@ -56,12 +56,21 @@ double ***applyFilter(double ***image, double **filter, int width, int height, i
 
 int main(int argc, char const *argv[])
 {
+    //Read input image
     CImg<unsigned char> originalImage("img/input.jpg"), visu(500, 400, 1, 3, 0);
+    /*
+      For store RGB image data int  3d double aaray
+      kernel[k][x][y]
+      Dimension 1 (K): for Red Blue Green (RGB) spectrum
+      Dimension 2 (x): pixels of image in width
+      Dimension 3 (y): pixels of image in height
+    */
     double ***kernel;
 
     int width = originalImage.width();
     int height = originalImage.height();
 
+    //Memory Allocation for kernel
     kernel = new double **[3];
     for (int i = 0; i < 3; i++)
     {
@@ -70,29 +79,35 @@ int main(int argc, char const *argv[])
             kernel[i][j] = new double[height];
     }
 
+    //Copy input image to kernel
     for (int k = 0; k < 3; k++)
         for (int x = 0; x < width; x++)
             for (int y = 0; y < height; y++)
                 kernel[k][x][y] = originalImage(x, y, 0, k);
 
+    //Showing orginal input image
     CImgDisplay Original_image(originalImage, "Original image");
 
-    /**/
+    /*---*/
     int filterHeight = 25;
     int filterWidth = 25;
+    //Apply Gaussian blur filter on input image
     kernel = applyFilter(kernel, getGaussian(filterHeight, filterWidth, 10.0), width, height, filterWidth, filterHeight);
-    /**/
+    /*---*/
 
     int bluredImageHeight = height - filterHeight + 1;
     int bluredImageWidth = width - filterWidth + 1;
+    //Create CImg object for convert double kernel to RGB image
     CImg<unsigned char> image(bluredImageWidth, bluredImageHeight, 1, 3);
 
+    //Copy blured image to image object
     for (int k = 0; k < 3; k++)
         for (int x = 0; x < bluredImageWidth; x++)
             for (int y = 0; y < bluredImageHeight; y++)
                 image(x, y, 0, k) = kernel[k][x][y];
 
     image.normalize(0, 255);
+    //Showing blured image
     CImgDisplay bluredImage(image, "Blured Image");
     while (!bluredImage.is_closed())
         bluredImage.wait();
