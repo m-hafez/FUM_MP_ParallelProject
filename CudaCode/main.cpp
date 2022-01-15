98153b6430881428ec16e0fa568daea06d55de43
MohammadHafez Yari <hafez.yari33@gmail.com>
2022-01-15 17:04:20 +0330

    Add RGB output

--- CudaCode/main.cpp
+++ CudaCode/main.cpp
@@ -6,48 +6,63 @@
 
 using namespace cimg_library;
 
-int main()
+int main(int argc, char const *argv[])
 {
     GBFilter filter;
-
-    CImg<unsigned char> originalImage("img/RE1LDN2.jpg"), visu(500, 400, 1, 3, 0);
-    const unsigned char red[] = {255, 0, 0}, green[] = {0, 255, 0}, blue[] = {0, 0, 255};
-    double **kernel;
-    const float color[] = {255., 255., 255.};
+    //Read input image
+    CImg<unsigned char> originalImage("img/input.jpg"), visu(500, 400, 1, 3, 0);
+    /*
+      For store RGB image data int  3d double aaray
+      kernel[k][x][y]
+      Dimension 1 (K): for Red Blue Green (RGB) spectrum
+      Dimension 2 (x): pixels of image in width
+      Dimension 3 (y): pixels of image in height
+    */
+    double ***kernel;
 
     int width = originalImage.width();
     int height = originalImage.height();
 
-    kernel = new double *[height];
-    for (int i = 0; i < height; i++)
+    //Memory Allocation for kernel
+    kernel = new double **[3];
+    for (int i = 0; i < 3; i++)
     {
-        kernel[i] = new double[width];
+        kernel[i] = new double *[width];
+        for (int j = 0; j < width; j++)
+            kernel[i][j] = new double[height];
     }
 
-    for (int y = 0; y < height; y++)
-    {
+    //Copy input image to kernel
+    for (int k = 0; k < 3; k++)
         for (int x = 0; x < width; x++)
-        {
-            kernel[y][x] = (double)originalImage(x, y);
-        }
-    }
+            for (int y = 0; y < height; y++)
+                kernel[k][x][y] = originalImage(x, y, 0, k);
 
-    CImg<unsigned char> image(width, height);
+    //Showing orginal input image
+    CImgDisplay Original_image(originalImage, "Original image");
+
     /**/
-    kernel = filter.applyFilter(kernel, filter.getGaussian(5, 5, 15.0), width, height);
+    int filterHeight = 25;
+    int filterWidth = 25;
+    //Apply Gaussian blur filter on input image
+    kernel = filter.applyFilter(kernel, filter.getGaussian(filterHeight, filterWidth, 10.0), width, height, filterWidth, filterHeight);
     /**/
 
-    for (int y = 0; y < height; y++)
-    {
-        for (int x = 0; x < width; x++)
-        {
-            image(x, y) = kernel[y][x];
-        }
-    }
+    int bluredImageHeight = height - filterHeight + 1;
+    int bluredImageWidth = width - filterWidth + 1;
+    //Create CImg object for convert double kernel to RGB image
+    CImg<unsigned char> image(bluredImageWidth, bluredImageHeight, 1, 3);
 
-    image.draw_point(50, 50, color);
+    //Copy blured image to image object
+    for (int k = 0; k < 3; k++)
+        for (int x = 0; x < bluredImageWidth; x++)
+            for (int y = 0; y < bluredImageHeight; y++)
+                image(x, y, 0, k) = kernel[k][x][y];
+
     image.normalize(0, 255);
-    image.save("output.jpg");
-
+    //Showing blured image
+    CImgDisplay bluredImage(image, "Blured Image");
+    while (!bluredImage.is_closed())
+        bluredImage.wait();
     return 0;
 }
