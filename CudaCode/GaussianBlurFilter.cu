41b615086570232600b9fced9749bcdd8c5fbe95
MohammadHafez Yari <hafez.yari33@gmail.com>
2022-01-15 17:04:59 +0330

    Add RGB output

--- CudaCode/GaussianBlurFilter.cu
+++ CudaCode/GaussianBlurFilter.cu
@@ -2,12 +2,6 @@
 #include <cuda.h>
 #include "GaussianBlurFilter.h"
 
-__global__ void cudaKernel(int *n, int limit)
-{
-
-}
-
-
 double **GBFilter::getGaussian(int height, int width, double sigma)
 {
     double **filter;
@@ -15,62 +9,45 @@ double **GBFilter::getGaussian(int height, int width, double sigma)
     int i, j;
 
     filter = new double *[height];
-    for (i = 0; i < height; i++)
-    {
+    for (int i = 0; i < height; i++)
         filter[i] = new double[width];
-    }
 
     for (i = 0; i < height; i++)
-    {
         for (j = 0; j < width; j++)
         {
             filter[i][j] = exp(-(i * i + j * j) / (2 * sigma * sigma)) / (2 * M_PI * sigma * sigma);
             sum += filter[i][j];
         }
-    }
 
     for (i = 0; i < height; i++)
-    {
         for (j = 0; j < width; j++)
-        {
             filter[i][j] /= sum;
-        }
-    }
 
     return filter;
 }
 
-double **GBFilter::applyFilter(double **image, double **filter, int width, int height)
+double ***GBFilter::applyFilter(double ***image, double **filter, int width, int height, int filterWidth, int filterHeight)
 {
-    int filterHeight = 5;
-    int filterWidth = 5;
+
     int newImageHeight = height - filterHeight + 1;
     int newImageWidth = width - filterWidth + 1;
-    int i, j, h, w;
+    int d, i, j, h, w;
 
-    double **newImage;
-    newImage = new double *[height];
+    double ***newImage;
+    newImage = new double **[3];
+    for (int i = 0; i < 3; i++)
     {
-        for (i = 0; i < height; i++)
-        {
-            newImage[i] = new double[width];
-        }
-
-        for (i = 0; i < newImageHeight; i++)
-        {
-            for (j = 0; j < newImageWidth; j++)
-            {
-                for (h = i; h < i + filterHeight; h++)
-                {
-                    for (w = j; w < j + filterWidth; w++)
-                    {
-                        newImage[i][j] += filter[h - i][w - j] * image[h][w];
-                    }
-                }
-            }
-        }
+        newImage[i] = new double *[width];
+        for (int j = 0; j < width; j++)
+            newImage[i][j] = new double[height];
     }
+
+    for (d = 0; d < 3; d++)
+        for (i = 0; i < newImageWidth; i++)
+            for (j = 0; j < newImageHeight; j++)
+                for (h = i; h < i + filterWidth; h++)
+                    for (w = j; w < j + filterHeight; w++)
+                        newImage[d][i][j] += filter[h - i][w - j] * image[d][h][w];
 
     return newImage;
 }
-
